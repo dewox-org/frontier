a3c818bb15fb84b3d32027f6578d6aab2d56fc2c
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2024-07-07 21:11:01 +0800

    random: Add random int, float, double generator

---
+++ source/random.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "type.hpp"
+
+namespace dewox::inline random
+{
+    auto random_identity(Identity* seed) -> Identity;  // [0, 1 << 64).
+    auto random_float32(Identity* seed) -> float;      // [0, 1).
+    auto random_float64(Identity* seed) -> double;     // [0, 1).
+}
+
